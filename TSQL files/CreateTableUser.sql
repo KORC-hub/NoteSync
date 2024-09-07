d1ab11b9485506e99971cf7e790eec2ea45efa93
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-09-07 17:27:37 -0500

    Added the files needed to create the tables and stored procedures in SQLSever

---
+++ TSQL files/CreateTableUser.sql
@@ -0,0 +1,45 @@
+USE NoteSyncDB
+
+CREATE TABLE [Membership] (
+	[MembershipId] TINYINT IDENTITY(1,1) PRIMARY KEY,
+	[MembershipName] VARCHAR(15) NOT NULL,
+	[Price] MONEY NOT NULL
+)
+
+--CREATE TABLE [Status] (
+--	[StatusId] TINYINT IDENTITY(1,1) PRIMARY KEY,
+--	[StatusName] VARCHAR(15) NOT NULL
+--)
+
+CREATE TABLE [User] (
+	[UserId] INT IDENTITY (1,1) PRIMARY KEY,
+	[UserNickname] NVARCHAR(15) NOT NULL,
+	[Email] NVARCHAR(100) NOT NULL,
+	[Password] NVARCHAR(60) NOT NULL, -- para bcrypt
+	[CreatedAt] DATETIME NOT NULL DEFAULT GETDATE(),
+	[ProfilePictureUrl] NVARCHAR(255),
+	[MembershipId] TINYINT NOT NULL DEFAULT 1,
+	--[StartMembershipDate] DATETIME,
+	--[EndMembershipDate] DATETIME, 
+	--[MembershipStatusId] TINYINT,
+	CONSTRAINT user_plan_fk FOREIGN KEY ([MembershipId]) REFERENCES [Membership]([MembershipId])
+	--CONSTRAINT user_status_fk FOREIGN KEY ([MembershipStatusId]) REFERENCES [Status]([StatusId])
+)
+
+
+-- Insert for membership table
+
+INSERT INTO [Membership] ([MembershipName], [Price]) 
+VALUES ('Basic', 0), 
+       ('Premium', 19.99), 
+       ('Student', 9.99) 
+
+--INSERT INTO [Status] ([StatusName]) 
+--VALUES ('Active'),
+--       ('Expired'), 
+--       ('Suspended'),
+--       ('Cancelled')
+
+
+SELECT * FROM [Membership]
+SELECT * FROM [User]
---
+++ TSQL files/StoredProceduresUser.sql
@@ -0,0 +1,79 @@
+-- Procedure to consult the table of users
+CREATE PROCEDURE SP_User_Index
+AS
+BEGIN
+  SELECT * FROM [User]
+END
+GO
+
+--Procedimiento para insertar socios
+CREATE PROCEDURE SP_User_Create
+(
+@UserNickname NVARCHAR(15), 
+@Email NVARCHAR(100),
+@Password NVARCHAR(60), 
+@MembershipId TINYINT
+)
+as
+BEGIN
+	INSERT INTO [User]([UserNickname],[Email],[Password],[MembershipId])
+	VALUES (@UserNickname, @Email, @Password,@MembershipId)
+END
+
+
+---Retorna el identificador de la tabla
+SELECT Scope_identity()
+GO
+
+CREATE PROCEDURE SP_User_Update
+(
+@UserId INT, 
+@UserNickname NVARCHAR(15), 
+@Email NVARCHAR(100),
+@Password NVARCHAR(60), 
+@ProfilePictureUrl NVARCHAR(255),
+@MembershipId TINYINT
+)
+AS
+BEGIN
+	Update [User] 
+	set [UserNickname]=@UserNickname, 
+		[Email]=@Email,
+		[Password]=@Password,
+		[ProfilePictureUrl]=@ProfilePictureUrl,
+		[MembershipId]=@MembershipId
+	where [UserId]=@UserId
+END
+
+
+---Retorna el identificador de la tabla
+Select Scope_identity()
+GO
+
+CREATE PROCEDURE SP_User_Delete
+(
+@UserId INT
+)
+AS
+BEGIN
+	DELETE FROM [User] WHERE [UserId]=@UserId
+END
+---Retorna el identificador de la tabla
+SELECT Scope_identity()
+GO
+
+CREATE PROCEDURE SP_User_Read
+(
+@UserId INT
+)
+AS
+BEGIN
+	SELECT * FROM [user] where [UserId]=@UserId
+END
+---Retorna el identificador de la tabla
+SELECT Scope_identity()
+GO
+
+SELECT name, object_id, create_date, modify_date
+FROM sys.procedures
+ORDER BY name;
