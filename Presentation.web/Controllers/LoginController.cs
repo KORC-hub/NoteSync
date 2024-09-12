391df8887abce033aa05390fd49389a3282d3d26
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-09-11 20:08:08 -0500

    A new project has been added to start the ASP.NET web implementation.

---
+++ Presentation.web/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.web.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+
+    }
+}
---
+++ Presentation.web/Controllers/RegisterController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.web.Controllers
+{
+    public class RegisterController : Controller
+    {
+    }
+}
