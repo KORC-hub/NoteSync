01c40f438449e6369d81b7b309d5c2ec5dc2d1dc
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-09-17 08:36:55 -0500

    Added specific classes for login

---
+++ Presentation.web/Models/DTOs/LoginUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.web.Models.DTOs
+{
+    public class LoginUserDTO
+    {
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Nickname { get; set; }
+        
+        [Required]
+        [Display(Name = "Correo")]
+        public string Email { get; set; }
+        
+        [Required]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Presentation.web/Models/VOs/LoggedInUserVO.cs
@@ -0,0 +1,4 @@
+﻿namespace Presentation.web.Models.VOs
+{
+    public record LoggedInUserVO(int UserId, string Nickname, string Email);
+}
