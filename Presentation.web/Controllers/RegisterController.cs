58b0884a2ec66382f8e025633c63363cf28e6de8
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-09-17 08:39:00 -0500

    Added specific drivers for login handling

--- Presentation.web/Controllers/RegisterController.cs
+++ Presentation.web/Controllers/RegisterController.cs
@@ -1,8 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities;
+using BusinessLogic.core;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace Presentation.web.Controllers
 {
     public class RegisterController : Controller
     {
+        #region Private Varieble
+
+        private UserBusinessLogic _userBusinessLogic = new UserBusinessLogic();
+        private BindingList<User> _users = new BindingList<User>();
+
+        #endregion
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var beer = new Beer()
+        //        {
+        //            BeerName = model.Name,
+        //            BrandId = model.BrandId,
+        //        };
+        //        _context.Beers.Add(beer);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["Brands"] = new SelectList(_context.Brands, "BrandId", "BrandName", model.BrandId);
+        //    return View(model);
+        //}
+
     }
 }
