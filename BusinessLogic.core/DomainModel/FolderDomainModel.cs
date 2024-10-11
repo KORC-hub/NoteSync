b399979338842f3e1ea2d314e4751060f024827b
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-10-10 23:42:18 -0500

    Domain Models have been added to the business logic layer.

---
+++ BusinessLogic.core/DomainModel/FolderDomainModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class FolderDomainModel
+    {
+        #region Private Variable
+
+        private int _fileId;
+        private DateTime _CreatedAt;
+        private DateTime _LastModifiedAt;
+        private int _userId;
+
+        #endregion
+
+        #region Public Variable
+
+        public int FileId { get => _fileId; set => _fileId = value; }
+        public DateTime CreatedAt { get => _CreatedAt; set => _CreatedAt = value; }
+        public DateTime LastModifiedAt { get => _LastModifiedAt; set => _LastModifiedAt = value; }
+        public int UserId { get => _userId; set => _userId = value; }
+
+        #endregion
+
+        #region Constructors
+        public FolderDomainModel()
+        {
+
+        }
+
+        public FolderDomainModel(int fileId, DateTime createdAt, DateTime lastModifiedAt, int userId)
+        {
+            _fileId = fileId;
+            _CreatedAt = createdAt;
+            _LastModifiedAt = lastModifiedAt;
+            _userId = userId;
+        }
+
+        #endregion
+    }
+}
---
+++ BusinessLogic.core/DomainModel/MembershipDomainModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+
+namespace Entities
+{
+    public class MembershipDomainModel
+    {
+        #region Private Variable
+
+        private byte _membershipId;
+        private string _membershipName;
+        private double _membershipPrice;
+
+        #endregion
+
+        #region Public Variable
+
+        public byte MembershipId1 { get => _membershipId; set => _membershipId = value; }
+        public string MembershipName1 { get => _membershipName; set => _membershipName = value; }
+        public double MembershipPrice1 { get => _membershipPrice; set => _membershipPrice = value; }
+
+        #endregion
+
+        #region Constructor
+
+        public MembershipDomainModel(byte MembershipId, string MembershipName, double MembershipPrice)
+        {
+            _membershipId = MembershipId;
+            _membershipName = MembershipName;
+            _membershipPrice = MembershipPrice;
+        }
+
+        #endregion
+
+    }
+}
