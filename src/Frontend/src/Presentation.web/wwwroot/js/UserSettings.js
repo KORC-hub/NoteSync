ceef16ae7ae732e55311fcc0b951bc82e6a53718
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-10-22 21:02:21 -0500

    Added styles for inputs with errors or verified inputs

---
+++ src/Frontend/src/Presentation.web/wwwroot/js/UserSettings.js
@@ -0,0 +1,26 @@
+﻿
+
+function DeleteUser(event, url) {
+    //event.preventDefault(); // Evita la redirección por defecto
+    //if (confirm("Are you sure to delete your account, this will also delete all your folders.")) {
+    //    // Si el usuario acepta, redirige al controlador
+    //    window.location.href = url;
+    //}
+    Swal.fire({
+        title: 'Delete user',
+        text: "Are you sure to delete your account, this will also delete all your folders.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel',
+        background: '#1e1e1e',  
+        color: '#fff', 
+        iconColor: '#ffd6a5',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = url;
+        }
+    });
+}
