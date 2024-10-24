e5ea509fa7f8508a4feb53f0be0aa139ae3be78b
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-10-24 15:56:37 -0500

    Added the dialog for the creation of folders

---
+++ src/Frontend/src/Presentation.web/wwwroot/js/dialog.js
@@ -0,0 +1,21 @@
+﻿const dialog = document.getElementById("myDialog");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementById("closeDialogBtn");
+
+// Abre el diálogo
+openBtn.addEventListener("click", () => {
+    console.log("a");
+    dialog.showModal(); // Muestra el diálogo como modal
+});
+
+// Cierra el diálogo al hacer clic en el botón de cerrar
+closeBtn.addEventListener("click", () => {
+    dialog.close(); // Cierra el diálogo
+});
+
+// Cierra el diálogo al hacer clic fuera del área del diálogo
+dialog.addEventListener("click", (event) => {
+    if (event.target === dialog) {
+        dialog.close();
+    }
+});
