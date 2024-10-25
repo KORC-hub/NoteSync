29f8f3e28af7666696bd77239b9600995187cc07
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-10-24 19:54:28 -0500

    Dependencies were added for each layer

---
+++ src/Backend/src/DataAccess.SqlServer/DataAccessDependecies.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Abstractions.Models;
+using DataAccess.Abstractions.Repositories.Specific;
+using DataAccess.SqlServer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.SqlServer
+{
+    public static class DataAccessDependecies
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<NoteSyncDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            // Registro de UserRepository
+            services.AddScoped<IUserRepository<IUser>, UserRepository>();
+            services.AddScoped<IMembershipRepository<IMembership>, MembershipRepository>();
+            services.AddScoped<ITagRepository<ITag>, TagRepository>();
+            services.AddScoped<IFolderRepository<IFolder>, FolderRepository>();
+            services.AddScoped<IFolderTagRepository<IFolderTag>, FolderTagRepository>();
+            services.AddScoped<IPageRepository<IPage>, PageRepository>();
+
+            return services;
+        }
+    }
+}
