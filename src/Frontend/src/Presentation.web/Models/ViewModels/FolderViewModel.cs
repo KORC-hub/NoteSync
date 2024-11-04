fa40a301e9689b47139526c3ab81a389ae04f50d
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-11-04 15:25:30 -0500

    Partial views were added to the main view of the Folders, in order to show the data obtained from the BLL.

---
+++ src/Frontend/src/Presentation.web/Models/ViewModels/FolderViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DTOs;
+
+namespace Presentation.web.Models.ViewModels
+{
+    public class FolderViewModel
+    {
+        public string? FolderName { get; set; }
+
+        public string? Taglist { get; set; }
+
+        public List<FolderDto>? Folders { get; set; }
+        public List<TagDto>? Tags { get; set; }
+        public string[]? Filters { get; set; }
+
+        public FolderViewModel()
+        {
+        }
+
+        public FolderViewModel(List<FolderDto>? folders, List<TagDto>? tags)
+        {
+            Folders = folders;
+            Tags = tags;
+        }
+
+    }
+}
