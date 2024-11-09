928a6841b900d4af9fcca7319434656e495cb26b
Rogers C. <72825328+KORC-hub@users.noreply.github.com>
2024-11-08 21:38:32 -0500

    Update NoteSyncTriggers.sql

--- Documentation/NoteSyncTriggers.sql
+++ Documentation/NoteSyncTriggers.sql
@@ -1,25 +1,6 @@
 USE NoteSyncDB
 
 GO
-CREATE TRIGGER trg_InsertUser
-ON [User]
-AFTER INSERT
-AS
-BEGIN
-    INSERT INTO [Tag] (UserId, TagContent, Color)
-    SELECT UserId, 'To do', '#84b6f4' 
-    FROM inserted;
-
-	INSERT INTO [Tag] (UserId, TagContent, Color)
-    SELECT UserId, 'In progress', '#fdfd96' 
-    FROM inserted;
-
-	INSERT INTO [Tag] (UserId, TagContent, Color)
-    SELECT UserId, 'Finish', '#77dd77' 
-    FROM inserted;
-END;
-
-GO
 CREATE TRIGGER trg_InsertFolder
 ON [Folder]
 AFTER INSERT
