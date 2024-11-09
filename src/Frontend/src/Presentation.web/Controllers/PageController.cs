22633271fdf3dd6c50fe562c9987c62a2a64bb73
Kevin Rogers <rogerscardonakevin@gmail.com>
2024-11-08 21:24:32 -0500

    Added controller for pages

---
+++ src/Frontend/src/Presentation.web/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.web.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
