cd21beed9f18045296167a77a76acba9a0f9ba16
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-24 23:09:33 -0500

    Add FolderVM class with properties and constructor

---
+++ src/Backend/src/API.core/Models/FolderVM.cs
@@ -0,0 +1,27 @@
+﻿using DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.core.Models
+{
+    public class FolderVM
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FolderName { get; set; }
+
+        //public List<TagDto>? Tags { get; set; }
+
+        #region Constructors
+
+        public FolderVM(int userId, string folderName)
+        {
+            UserId = userId;
+            FolderName = folderName;
+        }
+
+        #endregion
+    }
+}
