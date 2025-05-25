b6372623caf8fb0c6753461780d93a68b2a0c8de
Kevin Rogers <rogerscardonakevin@gmail.com>
2025-05-24 23:08:47 -0500

    Add folders API controller with GET and POST endpoints
    Introduced a new `foldersController` in `foldersController.cs` to manage folder-related operations. Key changes include:
    
    - Added `using` directives for required namespaces.
    - Defined the `foldersController` class with `[Route]` and `[ApiController]` attributes.
    - Implemented dependency injection for `IFolderUseCases`.
    - Added a `Get` endpoint to retrieve folders by `userId`.
    - Added a `Post` endpoint to create new folders with error handling.
    - Followed RESTful API principles for endpoint design.

---
+++ src/Backend/src/API.core/Controllers/foldersController.cs
@@ -0,0 +1,51 @@
+﻿using API.core.Models;
+using BusinessLogic.core.UseCases;
+using DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class foldersController : ControllerBase
+    {
+
+        private IFolderUseCases _folderService;
+
+        public foldersController(IFolderUseCases folderService)
+        {
+            _folderService = folderService;
+        }
+
+        // GET api/<foldersController>/UserId
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<FolderDto>> Get(int userId)
+        {
+            List<FolderDto> folders = new List<FolderDto>();
+            folders = await _folderService.GetAllFoldersAsync(userId);
+            return Ok(folders);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FolderDto>> Post(FolderVM folder)
+        {
+            try
+            {
+                FolderDto newfolder = new FolderDto();
+                newfolder.FolderName = folder.FolderName;
+                newfolder.UserId = folder.UserId;
+                newfolder.Tags = new List<TagDto>();
+                newfolder.FolderId = await _folderService.CreateFolder(newfolder);
+                return Ok(newfolder.FolderId);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+
+}
